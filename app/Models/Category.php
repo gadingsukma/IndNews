e1b9636c0680ead1896ae7b3c1230bcdd1582148
koala <gadingsukma12@gmail.com>
2025-05-25 08:17:35 +0700

    add CURD Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -15,6 +16,12 @@ class Category extends Model
         'slug',
         'icon',
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     public function news()
     {
